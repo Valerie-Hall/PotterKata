1abb69227d64743d1b848f3f154b814d9cfdcca9
Jadot29 <jagaines29@live.com>
2013-08-09 15:23:47 -0500

    After Refactor-Deleted PotterKata class--added BookHandler

---
+++ src/BookHandler.java
@@ -0,0 +1,51 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Thoughtworker
+ * Date: 8/9/13
+ * Time: 3:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BookHandler {
+    LinkedHashMap<String, Integer> bookNames = new LinkedHashMap<String, Integer>();
+
+    public BookHandler() {
+        initializeBooks();
+    }
+
+    private void initializeBooks() {
+        bookNames.put("Book 1", 1);
+        bookNames.put("Book 2", 2);
+        bookNames.put("Book 3", 2);
+        bookNames.put("Book 4", 2);
+        bookNames.put("Book 5", 2);
+        bookNames.put("Book 6", 1);
+        bookNames.put("Book 7", 0);
+    }
+
+    public Integer getTotalNumberOfBooks() {
+        Integer totalBooks = 0;
+        for (Integer quantity : bookNames.values()) {
+            totalBooks += quantity;
+        }
+        return totalBooks;
+    }
+
+
+    public double getTotalPrice() {
+        double totalPrice = 8 * getTotalNumberOfBooks();
+        return totalPrice;
+    }
+
+    public String toString() {
+        String receipt = "";
+        for (Map.Entry book : bookNames.entrySet()) {
+            receipt += book.getKey() + " - " + book.getValue() + "\n";
+        }
+        return receipt;
+    }
+
+
+}
---
+++ test/BookHandlerTest.java
@@ -0,0 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class BookHandlerTest {
+
+        BookHandler bookHandler;
+
+        @Before
+        public void setUp() {
+            bookHandler = new BookHandler();
+        }
+
+        @Test
+        public void shouldReturnPurchasedBookNames() {
+            assertThat(bookHandler.bookNames.keySet().toString(), is("[Book 1, Book 2, Book 3, Book 4, Book 5, Book 6, Book 7]"));
+        }
+
+        @Test
+        public void shouldReturnBookQuantities() {
+            assertThat(bookHandler.bookNames.values().toString(), is("[1, 2, 2, 2, 2, 1, 0]"));
+        }
+
+        @Test
+        public void shouldGetTotalNumberOfBooks() {
+            assertThat(bookHandler.getTotalNumberOfBooks(), is(10));
+        }
+
+        @Test
+        public void shouldGetTotalPriceOfBooks() {
+            assertThat(bookHandler.getTotalPrice(), is(80.));
+        }
+
+
+    }
+
